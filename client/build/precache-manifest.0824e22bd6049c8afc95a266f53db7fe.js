3aa01797bc14fecc3ff4454768ae185fe65fb1cd
Onoja <onojamatthew59@gmail.com>
2020-04-25 09:48:27 +0100

    added new updates

---
+++ client/build/precache-manifest.0824e22bd6049c8afc95a266f53db7fe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d665f05ee4f9bfdbaa8eebdb57cfa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd3901d631b956f1cd4a",
+    "url": "/static/css/2.7a762d8d.chunk.css"
+  },
+  {
+    "revision": "fbb9ae2b64694b7720ca",
+    "url": "/static/css/main.bb45c0c8.chunk.css"
+  },
+  {
+    "revision": "bd3901d631b956f1cd4a",
+    "url": "/static/js/2.20b7860c.chunk.js"
+  },
+  {
+    "revision": "d76db2e6ea424b917cb9ff0eb341aacb",
+    "url": "/static/js/2.20b7860c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbb9ae2b64694b7720ca",
+    "url": "/static/js/main.5aaebca6.chunk.js"
+  },
+  {
+    "revision": "e8807207f91b8ffa3524",
+    "url": "/static/js/runtime-main.34922f11.js"
+  },
+  {
+    "revision": "6394e7167a477ae2479c8f83daa3d4f3",
+    "url": "/static/media/logo-dark.6394e716.png"
+  },
+  {
+    "revision": "5845844574d1ebe07eec282fb076a1c5",
+    "url": "/static/media/man-shipping-box.58458445.jpg"
+  },
+  {
+    "revision": "1472b8ed91edbc14587927eb6bb97cc3",
+    "url": "/static/media/pallet-in-warehouse.1472b8ed.jpg"
+  },
+  {
+    "revision": "a1591f8eb1ebd5b087980236dc19d16d",
+    "url": "/static/media/shipping-cost.a1591f8e.jpg"
+  },
+  {
+    "revision": "ea483b16378e19b73b9cf9c3b8327184",
+    "url": "/static/media/signing-shipping.ea483b16.jpg"
+  },
+  {
+    "revision": "c75b80eefc2dd2f157d0ffda211912bd",
+    "url": "/static/media/trolley.c75b80ee.jpg"
+  }
+]);
